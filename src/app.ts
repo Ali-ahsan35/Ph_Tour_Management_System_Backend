f869afdc5b2f8a5ee35cfa72602f1ec661da67a9
Ali-ahsan35 <syedahsan748@gmail.com>
2025-09-02 08:41:59 +0600

    global error handler added

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,23 @@
 import express, { Request, Response } from "express";
+
+import cors from "cors";
+import { router } from "./app/routes";
+
+import { globalErrorhandler } from "./app/middlewares/globalErrorHandler";
+
 const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+app.use("/api/v1", router);
 
 app.get("/", (req: Request, res: Response) => {
     res.status(200).json({
         message: "Welcome too Tour Management System Backend"
     })
 });
+
+app.use(globalErrorhandler);
 
 export default app;
---
+++ src/app/modules/user/user.controller.ts
@@ -0,0 +1,25 @@
+import { NextFunction, Request, Response } from "express";
+// import { User } from "./user.model";
+import httpStatus from "http-status-codes"
+import { UserServices } from "./user.service";
+
+const createUser = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+
+        const user = await UserServices.createUser(req.body);
+
+        res.status(httpStatus.CREATED).json({
+            message: "User created successfully !",
+            user
+        })
+
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (err: any) {
+        console.log(err);
+        next(err);
+    }
+}
+
+export const UserControllers = {
+    createUser
+}
